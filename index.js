86c23b93f1fd4e9739cad76d0f0bf07d4b6fd2f0
parthpatel91 <86828757+parthpatel91@users.noreply.github.com>
2021-10-10 13:21:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const os = require("os");
+console.log("Operating system CPU architecture "+ os.arch()); //OS CPU architecture
+console.log("Free memory of the system "+ os.freemem()); //Free memory of the system
+console.log("Total memory of the system "+ os.totalmem()); //Total memory of the system
+console.log("Operating system platform "+ os.platform()); //OS Platform
+console.log("Information about the current user "+ os.userInfo()); //Information about the current user
